2cca2ad36b2e0a866eb7248e37e6ff46bafc7edb
U-LCGH\tom.monks <tom.monks@LISDATA.LCGH.LOCAL>
2019-10-16 15:12:09 -0400

    Updates

---
+++ precache-manifest.601b9d58baa6a3565fd700200d349ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12147ebb0890abdb9cb7ced3578aa492",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "e2d08d1cfe82c31cf9dc",
+    "url": "/pomodoro-timer/static/css/main.358f6edc.chunk.css"
+  },
+  {
+    "revision": "05e152e05ff94c8e9cb7",
+    "url": "/pomodoro-timer/static/js/2.9da9e92d.chunk.js"
+  },
+  {
+    "revision": "e2d08d1cfe82c31cf9dc",
+    "url": "/pomodoro-timer/static/js/main.a962eaf7.chunk.js"
+  },
+  {
+    "revision": "07b9b1055b34ffcbb52e",
+    "url": "/pomodoro-timer/static/js/runtime-main.5a3f11a7.js"
+  },
+  {
+    "revision": "7c531afcf9ff79b2713cd7aff4ecab25",
+    "url": "/pomodoro-timer/static/media/Twin-bell-alarm-clock.7c531afc.mp3"
+  }
+]);
