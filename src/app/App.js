728422e79aaf7691536801b8d1a031bb76ee6f63
Tom Monks <monksy@gmail.com>
2019-10-23 14:56:55 -0400

    Renamed action creators and moved to actions.js

--- src/app/App.js
+++ src/app/App.js
@@ -4,15 +4,13 @@ import Timer from "../timer/Timer";
 import Preset from "../preset/Preset";
 import { connect } from "react-redux";
 import soundfile from "../assets/beep-beep-bopbop-bop-bop.mp3";
+import * as actions from "./actions";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlayCircle, faPauseCircle, faUndoAlt } from "@fortawesome/free-solid-svg-icons";
+import { faUndoAlt } from "@fortawesome/free-solid-svg-icons";
 import { Helmet } from "react-helmet";
 
-let timerID = null;
 let audioRef = null;
-const MILLISECONDS = 1000;
-// const MILLISECONDS = 30;
 
 const formatTime = seconds => {
   let minutesLeft = Math.floor(seconds / 60);
@@ -30,10 +28,9 @@ function App(props) {
   /* clear interval on unmount */
   useEffect(() => {
     return () => {
-      // if (props.running) {
-      //   props.toggle();
-      // }
-      clearInterval(timerID);
+      if (props.running) {
+        props.toggle();
+      }
     };
   }, []);
 
@@ -53,9 +50,6 @@ function App(props) {
           value={props.presets[0].value}
           active={props.running && props.currentPreset == 0}
         />
-        {/* <button id="reset" onClick={props.reset}>
-            Reset
-          </button> */}
         <Preset
           index={1}
           id="break"
@@ -77,14 +71,6 @@ function App(props) {
       <div id="reset" onClick={() => props.reset(audioRef)}>
         <FontAwesomeIcon icon={faUndoAlt} />
       </div>
-      {/* <FontAwesomeIcon
-          id="start_stop"
-          onClick={props.toggle}
-          icon={props.running ? faPauseCircle : faPlayCircle}
-        /> */}
-      {/* <button id="start_stop" onClick={props.toggle}>
-          {props.running ? "Pause" : "Play"}
-        </button> */}
       <audio
         ref={input => {
           audioRef = input;
@@ -95,114 +81,6 @@ function App(props) {
     </div>
   );
 }
-
-/* redux-thunk action creator to switch to next preset */
-const nextPreset = () => {
-  return (dispatch, getState) => {
-    const { timer, presets } = getState();
-    let nextPreset = (timer.currentPreset + 1) % presets.length;
-
-    dispatch({
-      type: "NEXT_PRESET",
-      index: nextPreset,
-      timeLeft: presets[nextPreset].value * 60,
-      label: presets[nextPreset].label
-    });
-  };
-};
-
-const tick = myAudioRef => {
-  return (dispatch, getState) => {
-    const { timer } = getState();
-    if (timer.timeLeft <= 0) {
-      const { timer, presets } = getState();
-      let nextPreset = (timer.currentPreset + 1) % presets.length;
-      let audioPromise = myAudioRef.play();
-
-      if (audioPromise !== undefined) {
-        audioPromise
-          .then(() => {
-            console.log("RIIIINGG!");
-          })
-          .catch(error => {
-            console.log("audio playback failed: " + error);
-          });
-      }
-
-      dispatch({
-        type: "NEXT_PRESET",
-        index: nextPreset,
-        timeLeft: presets[nextPreset].value * 60,
-        label: presets[nextPreset].label
-      });
-    } else {
-      dispatch({ type: "TICK" });
-    }
-  };
-};
-
-const incrementAction = index => {
-  return (dispatch, getState) => {
-    const { presets, timer } = getState();
-    if (presets[index].value < 60) {
-      /* if it's the current timer being updated, also set the timer's time to match */
-      if (index === timer.currentPreset) {
-        dispatch({ type: "SET_TIME", payload: (presets[index].value + 1) * 60 });
-      }
-      dispatch({ type: "INCREMENT", id: index });
-    }
-  };
-};
-
-const decrementAction = index => {
-  return (dispatch, getState) => {
-    const { presets, timer } = getState();
-    if (presets[index].value > 1) {
-      /* if it's the current timer being updated, also set the timer's time to match */
-      if (index === timer.currentPreset) {
-        dispatch({ type: "SET_TIME", payload: (presets[index].value - 1) * 60 });
-      }
-      dispatch({ type: "DECREMENT", id: index });
-    }
-  };
-};
-
-const toggleTimerAction = myAudioRef => {
-  return (dispatch, getState) => {
-    const { timer } = getState();
-    if (!timer.running) {
-      timerID = setInterval(() => {
-        dispatch(tick(myAudioRef));
-      }, MILLISECONDS);
-      dispatch({ type: "START" });
-    } else {
-      clearInterval(timerID);
-      dispatch({ type: "STOP" });
-    }
-  };
-};
-
-const resetAction = myAudioRef => {
-  return (dispatch, getState) => {
-    const { timer } = getState();
-    let pausePromise = myAudioRef.pause();
-    if (pausePromise !== undefined) {
-      pausePromise
-        .then(() => {
-          console.log("stopping audio");
-        })
-        .catch(error => {
-          console.log("error stopping audio: " + error);
-        });
-    }
-    myAudioRef.currentTime = 0;
-    // audioRef.load();
-    if (timer.running) {
-      clearInterval(timerID);
-    }
-    dispatch({ type: "RESET" });
-  };
-};
 
 const mapStateToProps = state => {
   return {
@@ -218,11 +96,11 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     // tick: () => dispatch(tick(audioRef)),
-    toggle: () => dispatch(toggleTimerAction(audioRef)),
-    reset: () => dispatch(resetAction(audioRef)),
-    nextPreset: () => dispatch(nextPreset()),
-    increment: index => dispatch(incrementAction(index)),
-    decrement: index => dispatch(decrementAction(index))
+    toggle: () => dispatch(actions.toggle(audioRef)),
+    reset: () => dispatch(actions.reset(audioRef)),
+    nextPreset: () => dispatch(actions.nextPreset()),
+    increment: index => dispatch(actions.increment(index)),
+    decrement: index => dispatch(actions.decrement(index))
   };
 };
 
---
+++ src/app/actions.js
@@ -0,0 +1,111 @@
+let timerID = null;
+const MILLISECONDS = 1000;
+// const MILLISECONDS = 30;
+
+/* redux-thunk action creator to switch to next preset */
+export const nextPreset = () => {
+  return (dispatch, getState) => {
+    const { timer, presets } = getState();
+    let nextPreset = (timer.currentPreset + 1) % presets.length;
+
+    dispatch({
+      type: "NEXT_PRESET",
+      index: nextPreset,
+      timeLeft: presets[nextPreset].value * 60,
+      label: presets[nextPreset].label
+    });
+  };
+};
+
+export const tick = myAudioRef => {
+  return (dispatch, getState) => {
+    const { timer } = getState();
+    if (timer.timeLeft <= 0) {
+      const { timer, presets } = getState();
+      let nextPreset = (timer.currentPreset + 1) % presets.length;
+      let audioPromise = myAudioRef.play();
+
+      if (audioPromise !== undefined) {
+        audioPromise
+          .then(() => {
+            console.log("RIIIINGG!");
+          })
+          .catch(error => {
+            console.log("audio playback failed: " + error);
+          });
+      }
+
+      dispatch({
+        type: "NEXT_PRESET",
+        index: nextPreset,
+        timeLeft: presets[nextPreset].value * 60,
+        label: presets[nextPreset].label
+      });
+    } else {
+      dispatch({ type: "TICK" });
+    }
+  };
+};
+
+export const increment = index => {
+  return (dispatch, getState) => {
+    const { presets, timer } = getState();
+    if (presets[index].value < 60) {
+      /* if it's the current timer being updated, also set the timer's time to match */
+      if (index === timer.currentPreset) {
+        dispatch({ type: "SET_TIME", payload: (presets[index].value + 1) * 60 });
+      }
+      dispatch({ type: "INCREMENT", id: index });
+    }
+  };
+};
+
+export const decrement = index => {
+  return (dispatch, getState) => {
+    const { presets, timer } = getState();
+    if (presets[index].value > 1) {
+      /* if it's the current timer being updated, also set the timer's time to match */
+      if (index === timer.currentPreset) {
+        dispatch({ type: "SET_TIME", payload: (presets[index].value - 1) * 60 });
+      }
+      dispatch({ type: "DECREMENT", id: index });
+    }
+  };
+};
+
+export const toggle = myAudioRef => {
+  return (dispatch, getState) => {
+    const { timer } = getState();
+    if (!timer.running) {
+      timerID = setInterval(() => {
+        dispatch(tick(myAudioRef));
+      }, MILLISECONDS);
+      dispatch({ type: "START" });
+    } else {
+      clearInterval(timerID);
+      dispatch({ type: "STOP" });
+    }
+  };
+};
+
+export const reset = myAudioRef => {
+  return (dispatch, getState) => {
+    const { timer } = getState();
+    let pausePromise = myAudioRef.pause();
+    if (pausePromise !== undefined) {
+      pausePromise
+        .then(() => {
+          console.log("stopping audio");
+        })
+        .catch(error => {
+          console.log("error stopping audio: " + error);
+        });
+    }
+    myAudioRef.currentTime = 0;
+    // audioRef.load();
+    if (timer.running) {
+      clearInterval(timerID);
+    }
+    dispatch({ type: "RESET" });
+  };
+};
