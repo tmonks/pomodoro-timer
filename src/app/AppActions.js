28a5f31c20e848a243a2b0b384cfe9fecc17d639
Tom Monks <monksy@gmail.com>
2019-10-25 14:33:40 -0400

    Cleaned up AppActions

--- src/app/AppActions.js
+++ src/app/AppActions.js
@@ -2,13 +2,6 @@ export const increment = index => {
   return (dispatch, getState) => {
     const { presets, timer } = getState();
     if (presets[index].value < 60) {
-      /* if it's the current timer being updated, also set the timer's time to match */
-      // if (index === timer.currentPreset) {
-      //   dispatch({
-      //     type: "SET_TIME",
-      //     payload: (presets[index].value + 1) * 60
-      //   });
-      // }
       dispatch({ type: "INCREMENT", id: index });
     }
   };
@@ -18,31 +11,15 @@ export const decrement = index => {
   return (dispatch, getState) => {
     const { presets, timer } = getState();
     if (presets[index].value > 1) {
-      /* if it's the current timer being updated, also set the timer's time to match */
-      // if (index === timer.currentPreset) {
-      //   dispatch({
-      //     type: "SET_TIME",
-      //     payload: (presets[index].value - 1) * 60
-      //   });
-      // }
       dispatch({ type: "DECREMENT", id: index });
     }
   };
 };
 
-export const reset = myAudioRef => {
+export const reset = () => {
   return (dispatch, getState) => {
     const { timer } = getState();
-    let pausePromise = timer.audioRef.pause();
-    if (pausePromise !== undefined) {
-      pausePromise
-        .then(() => {
-          console.log("stopping audio");
-        })
-        .catch(error => {
-          console.log("error stopping audio: " + error);
-        });
-    }
+    timer.audioRef.pause();
     timer.audioRef.currentTime = 0;
 
     if (timer.running) {
