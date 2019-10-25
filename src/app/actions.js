f036edb01170f9bf67e17b177dbd2d447787cd72
Tom Monks <monksy@gmail.com>
2019-10-25 13:03:23 -0400

    Replaced SET_TIME with INCREMENT/DECREMENT actions on Timer

--- src/app/actions.js
+++ src/app/actions.js
@@ -36,12 +36,12 @@ export const increment = index => {
     const { presets, timer } = getState();
     if (presets[index].value < 60) {
       /* if it's the current timer being updated, also set the timer's time to match */
-      if (index === timer.currentPreset) {
-        dispatch({
-          type: "SET_TIME",
-          payload: (presets[index].value + 1) * 60
-        });
-      }
+      // if (index === timer.currentPreset) {
+      //   dispatch({
+      //     type: "SET_TIME",
+      //     payload: (presets[index].value + 1) * 60
+      //   });
+      // }
       dispatch({ type: "INCREMENT", id: index });
     }
   };
@@ -52,12 +52,12 @@ export const decrement = index => {
     const { presets, timer } = getState();
     if (presets[index].value > 1) {
       /* if it's the current timer being updated, also set the timer's time to match */
-      if (index === timer.currentPreset) {
-        dispatch({
-          type: "SET_TIME",
-          payload: (presets[index].value - 1) * 60
-        });
-      }
+      // if (index === timer.currentPreset) {
+      //   dispatch({
+      //     type: "SET_TIME",
+      //     payload: (presets[index].value - 1) * 60
+      //   });
+      // }
       dispatch({ type: "DECREMENT", id: index });
     }
   };
